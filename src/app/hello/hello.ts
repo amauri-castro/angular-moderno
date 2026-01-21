88a1438885116db9e25ef941d34621f7f3bc2f46
Amauri Castro <amauri.p.castro@gmail.com>
2026-01-21 14:05:54 -0300

    implementado contador utilizando writable signal

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -14,6 +14,21 @@ export class Hello {
   protected onClick() {
     console.log('toggled');
     this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  increaseCounter() {
+    //this.count = this.count + 1;
+    this.count.update(value => value + 1);
+  }
+
+  decreaseCounter() {
+    this.count.update(value => value - 1);
+  }
+
+  resetCounter() {
+    this.count.set(0);
   }
 
 }
